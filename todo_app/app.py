c2b32a06d878a16357232cc356aede8b39dff7ce
jfernandes7 <josephferns14@gmail.com>
2023-01-26 09:19:03 +0000

    loggly producing useful logs

--- todo_app/app.py
+++ todo_app/app.py
@@ -36,24 +36,28 @@ def home():
 def new_item():
     item = request.form["title"]
     add_card(item)
+    app.logger.info(f"Item {item} new item was added")
     return redirect(url_for("home"))
 
 @app.route('/doingitem', methods= ["POST"])
 def to_doing_item():
     item = request.form["doing_item_form"]
     doing_card(item)
+    app.logger.info(f"Item {item} was moved to doing")
     return redirect(url_for("home"))
 
 @app.route("/done_card", methods=["POST"])
 def complete_item():
     item = request.form["done_card_form"]
     done_card(item)
+    app.logger.info(f"Item {item} was moved to done")
     return redirect(url_for("home"))
 
 @app.route('/delete_card', methods=["POST"])
 def delete_item():
     item = request.form["delete_card"]
     delete_card(item)
+    app.logger.info(f"Item {item} was deleted")
     return redirect(url_for("home"))
     
  
